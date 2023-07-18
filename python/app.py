34f12bf6fb54444b2ae771f323bc524ee2d07153
elecun <elecun2@gmail.com>
2023-07-18 09:54:45 +0900

    interfacing message apis

--- python/app.py
+++ python/app.py
@@ -15,6 +15,7 @@ from PyQt6.uic import loadUi
 from PyQt6.QtCore import QObject, Qt, QTimer, QThread, pyqtSignal
 import timeit
 import paho.mqtt.client as mqtt
+from datetime import datetime
 
 
 WORKING_PATH = pathlib.Path(__file__).parent
@@ -30,6 +31,9 @@ btn_camera_open = {camera_ids[0]:"btn_camera_open_1",
                   camera_ids[2]:"btn_camera_open_3", 
                   camera_ids[3]:"btn_camera_open_4"}
 mqtt_topic_manager = "flame/avsim/manager"
+_def_record_fps = 30
+_def_record_width = 1920
+_def_record_height = 1080
 
 '''
 camera module with thread
@@ -40,13 +44,17 @@ class CameraModule(QThread):
     def __init__(self, camera_id):
         super().__init__()
         self.camera_id = camera_id
-        self.working = False
+        self.working = False # grabing image
+        self.record_start = False # recording video
 
     def open(self) -> bool:
         self.dev = cv2.VideoCapture(self.camera_id)
+        self.fourcc = cv2.VideoWriter_fourcc(*'mp4v') # high compression but smaller (file extension : mp4)
+        #self.fourcc = cv2.VideoWriter_fourcc(*'MJPG') # low compression but bigger (file extension : avi)
+        
         if not self.dev.isOpened():
-            # print("cannot connect camera device {}".format(self.camera_id))
             return False
+        
         self.working = True
         print("connected camera device : {}".format(self.camera_id))
         return True
@@ -57,7 +65,13 @@ class CameraModule(QThread):
                 break
             
             start_t = timeit.default_timer()
+        
             ret, frame = self.dev.read()
+            
+            # enable recording
+            if self.record_start:
+                self.video_out.write(frame)
+                
             if not ret:
                 self.working = False
                 self.dev.release()
@@ -69,7 +83,7 @@ class CameraModule(QThread):
             # post processing
             end_t = timeit.default_timer()
             FPS = int(1./(end_t - start_t ))
-            cv2.putText(rgb_image, "Camera #{}({})".format(self.camera_id, FPS), (10,50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2, cv2.LINE_AA)
+            cv2.putText(rgb_image, "Camera #{}(fps:{})".format(self.camera_id, FPS), (10,50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2, cv2.LINE_AA)
 
             h, w, ch = rgb_image.shape
             bytes_per_line = ch * w
@@ -80,11 +94,34 @@ class CameraModule(QThread):
     def close(self):
         if self.dev:
             self.requestInterruption()
+            if self.record_start:
+                self.video_out.release()
             self.dev.release()
+            
+    def pause_recording(self):
+        self.record_start = False
+    
+    def stop_recording(self):
+        if self.dev:
+            self.record_start = False
+            self.video_out.release()
+            
+        
+    def start_recording(self):
+        if not self.record_start:
+            self.record_start_datetime = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+            self.camera_fps = self.dev.get(cv2.CAP_PROP_FPS)
+            self.camera_w = int(self.dev.get(cv2.CAP_PROP_FRAME_WIDTH))
+            self.camera_h = int(self.dev.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            print(self.dev.get(cv2.CAP_PROP_FOURCC))
+            print("recording camera({}) info : ({},{}@{})".format(self.camera_id, self.camera_w, self.camera_h, self.camera_fps))
+            self.video_out = cv2.VideoWriter('./video/{}_cam_{}.mp4'.format(self.record_start_datetime, self.camera_id), self.fourcc, _def_record_fps, (self.camera_w, self.camera_h))
+            self.record_start = True
+        else:
+            print("Already recording...")
     
     def begin(self):
         if self.dev.isOpened():
-            print("start camera thread", self.camera_id)
             self.start()
             
     def __str__(self):
@@ -100,14 +137,17 @@ class CameraMonitor(QMainWindow):
         loadUi(APP_UI, self)
 
         self.device_modules = {}
+        self.sub_api = {
+            "flame/avsim/cam/record_start" : self.api_start_record,
+            "flame/avsim/cam/record_stop" : self.api_stop_record
+        }
         
         self.mq_client = mqtt.Client(client_id="flame-avsim-cam",transport='tcp',protocol=mqtt.MQTTv311, clean_session=True)
         self.mq_client.on_connect = self.on_mqtt_connect
         self.mq_client.on_message = self.on_mqtt_message
+        self.mq_client.on_disconnect = self.on_mqtt_disconnect
         self.mq_client.connect_async("127.0.0.1",port=1883,keepalive=60)
         self.mq_client.loop_start()
-        
-        print("start mqtt")
 
         # for manual load
         for id in camera_ids:
@@ -120,18 +160,34 @@ class CameraMonitor(QMainWindow):
             else:
                 btn = self.findChild(QPushButton, btn_camera_open[id])
                 btn.clicked.connect(lambda:QMessageBox.critical(self, "No Camera", "No Camera device connection"))
+                
+    # message-based api
+    def api_start_record(self):
+        for dev in self.device_modules.values():
+            dev.start_recording()
+        self.show_on_statusbar("Start Recording...")
+            
+    def api_stop_record(self):
+        for dev in self.device_modules.values():
+            dev.stop_recording()
+        self.show_on_statusbar("Stopped Recording...")
+        
+                
+    # show message on status bar
+    def show_on_statusbar(self, text):
+        self.statusBar().showMessage(text)
     
 
+    # update image frame on label area
     def update_frame(self, image):
-
         id = self.sender().camera_id
         pixmap = QPixmap.fromImage(image)
         window = self.findChild(QLabel, camera_windows[id])
         window.setPixmap(pixmap.scaled(window.size(), Qt.AspectRatioMode.KeepAspectRatio))
 
+    # close event callback function by user
     def closeEvent(self, a0: QCloseEvent) -> None:
         for device in self.device_modules.values():
-            print("close ", device)
             device.close()
 
         return super().closeEvent(a0)
@@ -139,18 +195,30 @@ class CameraMonitor(QMainWindow):
     # MQTT sub functions
     def mqtt_startup_notify(self):
         if self.mq_client.is_connected():
-            msg = {"app":"flame-avsim-cam", "active":True}
+            msg = {"app":"flame-avsim-cam", "live":True}
             self.mq_client.publish(mqtt_topic_manager, json.dumps(msg), 0)
+        else:
+            self.show_on_statusbar("Notified")
     
     # MQTT callbacks
     def on_mqtt_connect(self, mqttc, obj, flags, rc):
         self.mqtt_startup_notify()
-        self.mq_client.subscribe("flame/avsim/status", 0)
-        print("rc: "+str(rc))
+        
+        # subscribe message api
+        for topic in self.sub_api.keys():
+            self.mq_client.subscribe(topic, 0)
+        
+        self.show_on_statusbar("Connected to Broker({})".format(str(rc)))
+        
+    def on_mqtt_disconnect(self, mqttc, userdata, rc):
+        self.show_on_statusbar("Disconnected to Broker({})".format(str(rc)))
         
     def on_mqtt_message(self, mqttc, userdata, msg):
-        print(" Received message " + str(msg.payload)+ " on topic '" + msg.topic+ "' with QoS " + str(msg.qos))
-    
+        if msg.topic in self.sub_api.keys():
+            self.sub_api[msg.topic]()
+        
+        # print(" Received message " + str(msg.payload)+ " on topic '" + msg.topic+ "' with QoS " + str(msg.qos))
+   
         
         
 
